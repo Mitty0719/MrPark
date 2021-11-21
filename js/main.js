01f9bc038c021f6d06ffcbb8245406cbd0fa8465
Mitty0719 <sprout071900@gmail.com>
2021-11-21 20:46:45 +0900

    add Mouseover Event to card

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-"use strict"
 (function(){
-    
+    "use strict"
+    const cardSpace = document.querySelector(".cardSpace");
+    const cards = document.querySelectorAll(".card");
+    cardSpace.addEventListener("click", zoomCard);
+
+    function zoomCard(e){
+        const elem = e.target;
+        
+        while(!elem.parentNode.classList.contains("card")){
+            if(elem.nodeName == "BODY"){
+                elem = null;
+                return;
+            }
+            elem.style.background = "red";
+        }
+    }
 })()
