bab3e894fb246e0d4e3b17260beb625c32a0213c
Mitty0719 <sprout071900@gmail.com>
2021-11-27 15:25:37 +0900

    ReDesign html, css

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 (function(){
     "use strict"
-    const cardSpace = document.querySelector(".cardSpace");
-    const cards = document.querySelectorAll(".card");
-    cardSpace.addEventListener("click", zoomCard);
-
-    function zoomCard(e){
-        const elem = e.target;
-        
-        while(!elem.parentNode.classList.contains("card")){
-            if(elem.nodeName == "BODY"){
-                elem = null;
-                return;
-            }
-            elem.style.background = "red";
-        }
-    }
+    
+    
 })()
