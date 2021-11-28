95e56953ec884ca91104a36b12903e8944119ee9
Mitty0719 <sprout071900@gmail.com>
2021-11-28 21:04:09 +0900

    Setted random Top style to rowText type of Right, Left

--- js/Word.js
+++ js/Word.js
@@ -10,8 +10,16 @@ function Word (row, str) {
     
     this.rowText = row.querySelector('.rowText');
 
+    // 텍스트 이동 에니메이션 시간 값
     this.rowText.style.animationDuration = ((Math.random() * 1000) % 10 + 8) + 's'; // % 범위 + 최소값
-    console.log(this.rowText.style.animationDuration);
+    
+    if(this.direction == 'Left' || this.direction == 'Right'){
+        // 상단으로 부터 위치
+        let topRate = (Math.random() * 100) % 90;
+        this.rowText.style.top = topRate + 'vh';
+        // 폰트 사이즈 수정
+        this.rowText.style.fontSize = '15vw';
+    }
 }
 
 Word.prototype = {
