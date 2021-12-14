8e44b7b66d025110c86fcae5bd7b45fc676cb1bf
Mitty0719 <sprout071900@gmail.com>
2021-12-14 21:31:09 +0900

    Added background Image of Content3 canvasBall(not matched size to window)

--- src/js/content.js
+++ src/js/content.js
@@ -223,12 +223,17 @@
     };
 
     // Content 3 - FaceBall
+    let img = new Image;
+    img.src = '/images/gugue.png';
+    img.width = '100vw';
+    img.htight = '100vh';
     function makeBall (e) {
         ballList.push(new Ball(e.clientX, e.clientY, canvasBall, ctx))
     };
     function animateBall () { // 매 프레임마다 벌어지는 일들
         ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
         ctx.fillRect(0, 0, canvasBall.width, canvasBall.height); // 캔버스 전체를 색칠해서 내용 지움
+        ctx.drawImage(img, 0, 0);
         ballList.forEach(function(ball){
             ball.update(); // ball1의 정보를 업데이트
             ball.draw(); // ball1 새로 그림
@@ -239,9 +244,9 @@
     function releaseContent3 () {
         canvasBall.width = window.innerWidth;
         canvasBall.height = window.innerHeight;
-
         canvasBall.addEventListener('click', makeBall);
         animateBall();
+        console.log(img);
     }
     function unreleaseContent3 () {
         window.cancelAnimationFrame(content3TimeId);
