6b61b03cb754e38110bbd9edb177eb84ff9df199
Mitty0719 <sprout071900@gmail.com>
2021-12-14 14:59:52 +0900

    Modify JS code for Performance Optimization

--- src/js/content.js
+++ src/js/content.js
@@ -33,12 +33,17 @@
 
     const replyCon = document.querySelector('.replyCon');
 
+    let content2TimeId = 0, content3TimeId = 0, content4TimeId = -1;
+
     // window resize
     window.addEventListener('resize', function(e){
         instaItemWidth = parseInt(window.getComputedStyle(instaItems).getPropertyValue('width'));
     });
 
     // content 설정
+    let selectedContentIndex = -1;
+    const release = [releaseContent1, releaseContent2, releaseContent3, releaseContent4, releaseContent5];
+    const unrelease = [unreleaseContent1, unreleaseContent2, unreleaseContent3, unreleaseContent4, unreleaseContent5];
     function showContent (e) {
         // contentCon.style.display = 'block';
         let elem = e.target;
@@ -50,39 +55,25 @@
             elem = elem.parentNode;
         }
         // contentCon.style.top = (parseInt(target.dataset.index)-1) * 20 + 'vh';
+        selectedContentIndex = elem.dataset.index;
 
         contentCon.dataset.state = 'open';
-        contentCon.dataset.openIndex = elem.dataset.index;
-        // console.log(elem.dataset.index)
-    }
+        contentCon.dataset.openIndex = selectedContentIndex;
+
+        release[selectedContentIndex-1]();
+    };
     function closeContent(e) {
         contentCon.dataset.state = 'close';
-        // filterCon 닫기
-        hideFilterCon();
 
-        // youtube 댓글 초기화
-        commentText.value = '';
-        canSubmit = false;
-        commentSubmit.classList.remove('activeSubmit');
-
-        // ballList 초기화
-        setTimeout(function(){
-            ballList.length = 0;
-        }, 1000);
-
-        // instaItem Index 초기화
-        resetInstaIndex();
-
-        stopVideo(instaVideo);
-        stopVideo(youtubeVideo);
-    }
+        unrelease[selectedContentIndex-1]();
+    };
     function stopVideo(video){
         video.pause();
         video.currentTime = 0;
         video.load();
-    }
+    };
 
-    galleryGround.addEventListener( 'click', showContent );
+    galleryGround.addEventListener( 'click', showContent);
     closeBox.addEventListener('click', closeContent)
     
     // clickEvent 위치 mouseCurosr 색생변경
@@ -108,41 +99,28 @@
 
     //     this.style.filter = contrast + hueRotate + saturate;
     // };
-
-    monaFrame.addEventListener('click', showFilterCon);
-
     function showFilterCon (e) {
         filterCon.style.top = 0;
-    }
+    };
     function hideFilterCon (e) {
         filterCon.style.top = -100 + 'vh';
-    }
+    };
+    function releaseContent1 () {
+        monaFrame.addEventListener('click', showFilterCon);
+    };
+    function unreleaseContent1 () {
+        hideFilterCon();
+        monaFrame.removeEventListener('click', showFilterCon);
+    };
 
     // Content 2 - Youtube
     let youtubeX = 0, youtubeY = 0;
     let youtubeCurrentX = 0, youtubeCurrentY = 0;
     let youtubeSpeed = 0.005;
+
     function moveBackground (e) {
         youtubeX = e.clientX - window.innerWidth / 2;
         youtubeY = e.clientY - window.innerHeight / 2;
-    }
-    function moveBackgroundRaf () {
-        youtubeCurrentX += (youtubeX - youtubeCurrentX) * youtubeSpeed;
-        youtubeCurrentY += (youtubeY - youtubeCurrentY) * youtubeSpeed;
-        backImage.style.transform = 'translate(' + - (youtubeCurrentX/20) + 'px, ' + -(youtubeCurrentY/20) + 'px)';
-        window.requestAnimationFrame(moveBackgroundRaf); 
-    }
-    moveBackgroundRaf();
-    
-    function activeCommentSubmit (e) {
-        if(commentText.value.trim() != ''){
-            canSubmit = true;
-            commentSubmit.classList.add('activeSubmit');
-        }
-        else {
-            canSubmit = false;
-            commentSubmit.classList.remove('activeSubmit');
-        }
     };
     function submitComment (e) {
         const text = commentText.value;
@@ -166,25 +144,54 @@
         commentItem.classList.add('commentItem');
         commentItem.innerHTML = commentItemHtml;
         commentList.append(commentItem);
-
-    }
+    };
     function cancelComment (e) {
         commentText.value = '';
         commentSubmit.classList.remove('activeSubmit');
-    }
+    };
+    function activeCommentSubmit (e) {
+        if(commentText.value.trim() != ''){
+            canSubmit = true;
+            commentSubmit.classList.add('activeSubmit');
+        }
+        else {
+            canSubmit = false;
+            commentSubmit.classList.remove('activeSubmit');
+        }
+    };
+    function moveBackgroundRaf () {
+        youtubeCurrentX += (youtubeX - youtubeCurrentX) * youtubeSpeed;
+        youtubeCurrentY += (youtubeY - youtubeCurrentY) * youtubeSpeed;
+        backImage.style.transform = 'translate(' + - (youtubeCurrentX/20) + 'px, ' + -(youtubeCurrentY/20) + 'px)';
+        content2TimeId = window.requestAnimationFrame(moveBackgroundRaf); 
+    };
+    function releaseContent2 () {
+        moveBackgroundRaf();
+        
+        contentItem2.addEventListener('mousemove', moveBackground);
+        commentText.addEventListener('keyup', activeCommentSubmit);
+        commentCancel.addEventListener('click', cancelComment);
+        commentSubmit.addEventListener('click', submitComment);
+    };
+    function unreleaseContent2 () {
+        window.cancelAnimationFrame(content2TimeId);
 
-    contentItem2.addEventListener('mousemove', moveBackground);
-    commentText.addEventListener('keyup', activeCommentSubmit);
-    commentCancel.addEventListener('click', cancelComment);
-    commentSubmit.addEventListener('click', submitComment);
+        contentItem2.removeEventListener('mousemove', moveBackground);
+        commentText.removeEventListener('keyup', activeCommentSubmit);
+        commentCancel.removeEventListener('click', cancelComment);
+        commentSubmit.removeEventListener('click', submitComment);
+
+        // youtube 댓글입력창 초기화
+        commentText.value = '';
+        canSubmit = false;
+        commentSubmit.classList.remove('activeSubmit');
+        stopVideo(youtubeVideo);
+    };
 
     // Content 3 - FaceBall
-    canvasBall.width = window.innerWidth;
-    canvasBall.height = window.innerHeight;
     function makeBall (e) {
         ballList.push(new Ball(e.clientX, e.clientY, canvasBall, ctx))
     };
-
     function animateBall () { // 매 프레임마다 벌어지는 일들
         ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
         ctx.fillRect(0, 0, canvasBall.width, canvasBall.height); // 캔버스 전체를 색칠해서 내용 지움
@@ -192,10 +199,25 @@
             ball.update(); // ball1의 정보를 업데이트
             ball.draw(); // ball1 새로 그림
         })
-        requestAnimationFrame(animateBall);
+        content3TimeId = window.requestAnimationFrame(animateBall);
     };
-    canvasBall.addEventListener('click', makeBall);
-    animateBall();
+    
+    function releaseContent3 () {
+        canvasBall.width = window.innerWidth;
+        canvasBall.height = window.innerHeight;
+
+        canvasBall.addEventListener('click', makeBall);
+        animateBall();
+    }
+    function unreleaseContent3 () {
+        window.cancelAnimationFrame(content3TimeId);
+        canvasBall.removeEventListener('click', makeBall);
+
+        // ballList 초기화
+        setTimeout(function(){
+            ballList.length = 0;
+        }, 1000);
+    }
 
     // Content 4 - Instagram
     // video 태그 drag 이벤트 안먹어서 click으로 수정
@@ -221,15 +243,16 @@
     let instaX = 0, instaY = 0;
     let instaCurrentX = 0, instaCurrentY = 0;
     let instaSpeed = 0.015;
+
     function rotateInstaCon (e) {
         instaX = e.clientX - window.innerWidth / 2;
         instaY = e.clientY - window.innerHeight / 2;
-    }
+    };
     function rotateInstaConRaf () {
         instaCurrentX += (instaX - instaCurrentX) * instaSpeed;
         instaCurrentY += (instaY - instaCurrentY) * instaSpeed;
         instaBox.style.transform = 'rotateX(' + -(instaCurrentY / 20) + 'deg) rotateY(' + (instaCurrentX / 20) + 'deg)';
-        window.requestAnimationFrame(rotateInstaConRaf);;
+        content4TimeId = window.requestAnimationFrame(rotateInstaConRaf);;
     }
     function moveInstaItem (e) {
         instaItemIndex++;
@@ -244,10 +267,21 @@
         stopVideo(instaVideo);
         instaItems.style.left = '-' + (instaItemWidth * instaItemIndex) + 'px';
     };
-    
-    rotateInstaConRaf();
-    contentItem4.addEventListener('mousemove', rotateInstaCon)
-    instaItemCon.addEventListener('click', moveInstaItem);
+    function releaseContent4 () {
+        
+        rotateInstaConRaf();
+        contentItem4.addEventListener('mousemove', rotateInstaCon)
+        instaItemCon.addEventListener('click', moveInstaItem);
+    }
+    function unreleaseContent4 () {
+        window.cancelAnimationFrame(content4TimeId);
+        contentItem4.removeEventListener('mousemove', rotateInstaCon)
+        instaItemCon.removeEventListener('click', moveInstaItem);
+
+        // instaItem Index 초기화
+        resetInstaIndex();
+        stopVideo(instaVideo);
+    }
 
     // Content5 - 지하철 광고판
     const replyList = ['오늘 누구 생일임?',
@@ -266,8 +300,9 @@
                        '와 전광판 보소'];
     const colorList = ['red', 'blue', 'crimson', 'orange', 'yellow', 'greenyellow']
     let replyCnt = 0;
+    let replyConst;
     function createReply(){
-        if(replyCon.childNodes.length > replyList.length){
+        if(replyCon.childNodes.length >= replyList.length){
             clearInterval(replyConst);
             return;
         }
@@ -283,6 +318,15 @@
 
         replyCnt++;
     }
-    let replyConst = setInterval(createReply, 3000);
+    function releaseContent5 () {
+        replyConst = setInterval(createReply, 3000);
+    }
+
+    function unreleaseContent5 () {
+        clearInterval(replyConst);
+        const replys = document.querySelectorAll('.replyItem');
+        replys.forEach(reply => replyCon.removeChild(reply));
+        replyCnt = 0;
+    }
 
 })()
