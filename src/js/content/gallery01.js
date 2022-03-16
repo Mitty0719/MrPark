410c80434eabb8a998d16ac8ef5c3492328628b7
Mitty0719 <sprout071900@gmail.com>
2022-03-16 15:22:47 +0900

    Modify item of gallery01 png to svg

--- src/js/content/gallery01.js
+++ src/js/content/gallery01.js
@@ -15,33 +15,33 @@ export class Gallery01{
 
   showGeometryCon(){
     this.geometryCon.classList.add('activate');
-    this.createGeometryItem(20);
+    this.createGeometryItem(40);
   }
 
   createGeometryItem(index){
-    let cnt = 0;
-    const createId = setInterval(()=>{
-      if(cnt++ < index){
-        const item = document.createElement('div');
-        const scale = 1.5 / cnt;
+    for(let i = 1; i <= index; i++){
+      const item = document.createElement('div');
+      const img = document.createElement('img');
+      img.src = './images/geometry.svg';
+      const scale = 1.2 / i;
 
-        item.classList.add('geometryItem');
-        item.style.transform = `scale(${scale})`;
-        this.geometryCon.appendChild(item);
-        this.geometryItems.push(item);
-        this.geometryScale.push(scale);
-      } else {
-        clearInterval(createId);
-      }
-    }, 200);
+      img.classList.add('geometryImg');
+      item.classList.add('geometryItem');
+      item.style.transform = `scale(${scale})`;
+
+      item.appendChild(img);
+      this.geometryCon.appendChild(item);
+      this.geometryItems.push(item);
+      this.geometryScale.push(scale);
+    }
   }
   moveGeometryCon(e){
     const x = e.clientX - (this.stageWidth / 2);
     const y = e.clientY - (this.stageHeight / 2);
 
     this.geometryItems.forEach((item, index) => {
-      const moveX = x / 2;
-      const moveY = y / 2;
+      const moveX = -(x / 3);
+      const moveY = -(y / 3);
       item.style.transform = `scale(${this.geometryScale[index]}) translate(${moveX}px, ${moveY}px)`;
     })
   }
--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,30 @@
-import { Word } from "./word.js";
-import { Gallery01 } from "./content/gallery01.js";
-import { Gallery02 } from "./content/gallery02.js";
-import { Gallery03 } from "./content/gallery03.js";
-import { Gallery04 } from "./content/gallery04.js";
-import { Gallery05 } from "./content/gallery05.js";
-
-class App{
-  constructor(){
+import {
+  Word
+} from "./module/word.js";
+import {
+  Gallery01
+} from "./content/gallery01.js";
+import {
+  Gallery02
+} from "./content/gallery02.js";
+import {
+  Gallery03
+} from "./content/gallery03.js";
+import {
+  Gallery04
+} from "./content/gallery04.js";
+import {
+  Gallery05
+} from "./content/gallery05.js";
+class App {
+  constructor() {
     this.rowSpace = document.querySelector('.rowSpace');
     this.rows = document.querySelectorAll('.row');
     this.galleryGround = document.querySelector('.galleryGround');
     this.contentCon = document.querySelector('.contentCon');
     this.closeBox = document.querySelector('.closeBox');
     this.rowStr = ['DancingMachine', 'WookeBox', 'BlueBlood', 'GunMulJu', 'Bbang', 'TensorFlow', 'GSP'];
-    
+
     this.rowSpace.addEventListener('mouseover', this.moveWords.bind(this));
     this.galleryGround.addEventListener('click', this.showContent.bind(this));
     this.closeBox.addEventListener('click', this.closeContent.bind(this));
@@ -21,14 +32,14 @@ class App{
     this.resize();
     this.setRow();
   }
-  resize(){
+  resize() {
     this.stageWidth = window.innerWidth;
     this.stageHeight = window.innerHeight;
   }
   showContent(e) {
     let elem = e.target;
-    while(!elem.classList.contains('galleryImg')){
-      if(elem.nodeName == 'BODY'){
+    while (!elem.classList.contains('galleryImg')) {
+      if (elem.nodeName == 'BODY') {
         elem = null;
         return;
       }
@@ -45,8 +56,8 @@ class App{
     this.contentCon.dataset.state = 'close';
     this.contentCon.dataset.openIndex = 0;
   };
-  setContent(){
-    switch(this.currentContentIndex){
+  setContent() {
+    switch (this.currentContentIndex) {
       case '1':
         this.currentContent = new Gallery01(this.stageWidth, this.stageHeight);
         break;
@@ -66,23 +77,25 @@ class App{
   }
 
   // word effect 설정
-  moveWords (e) {
+  moveWords(e) {
     let elem = e.target;
-    if(elem.classList.contains('rowChar')){
-        elem.classList.add('rowCharMove');
-        setTimeout(() => { elem.classList.remove('rowCharMove'); }, 2000);
+    if (elem.classList.contains('rowChar')) {
+      elem.classList.add('rowCharMove');
+      setTimeout(() => {
+        elem.classList.remove('rowCharMove');
+      }, 2000);
     };
   }
 
-  setRow(){
+  setRow() {
     this.rows.forEach((row, index) => {
       if (!row.classList.contains('galleryRow')) {
         new Word(row, this.rowStr[index], index);
-        
-        if( index % 2 == 0 ) {
-          row.classList.add( 'rowEven' );
+
+        if (index % 2 == 0) {
+          row.classList.add('rowEven');
         } else {
-          row.classList.add( 'rowOdd' );
+          row.classList.add('rowOdd');
         }
       }
     });
