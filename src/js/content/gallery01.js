29d30a50d74a4a30e82d1803494ef33db2bc0200
Mitty0719 <sprout071900@gmail.com>
2022-03-18 09:55:52 +0900

    Modify event type mousemove into click in gallery01

--- src/js/content/gallery01.js
+++ src/js/content/gallery01.js
@@ -1,3 +1,5 @@
+import { getRandomSign } from "../util.js";
+
 export class Gallery01{
   constructor(stageWidth, stageHeight){
     this.frame = document.querySelector('.monaFrame');
@@ -5,44 +7,53 @@ export class Gallery01{
     this.description = document.querySelector('.monaDesc p');
     this.geometryCon = document.querySelector('.geometryCon');
     this.geometryItems = [];
-    this.geometryScale = [];
-    this.stageWidth = stageWidth;
-    this.stageHeight = stageHeight;
+    this.resize(stageWidth, stageHeight);
 
     this.frame.addEventListener('click', this.showGeometryCon.bind(this));
-    document.addEventListener('mousemove', this.moveGeometryCon.bind(this));
+  }
+
+  resize(stageWidth, stageHeight){
+    this.stageWidth = stageWidth;
+    this.stageHeight = stageHeight;
   }
 
   showGeometryCon(){
     this.geometryCon.classList.add('activate');
-    this.createGeometryItem(40);
+    window.addEventListener('click', this.createGeometryItem.bind(this));
   }
 
-  createGeometryItem(index){
-    for(let i = 1; i <= index; i++){
-      const item = document.createElement('div');
-      const img = document.createElement('img');
-      img.src = './images/geometry.svg';
-      const scale = 1.2 / i;
+  createGeometryItem(){
+    const item = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = './images/geometry.svg';
+    let scale, x, y;
 
-      img.classList.add('geometryImg');
-      item.classList.add('geometryItem');
-      item.style.transform = `scale(${scale})`;
-
-      item.appendChild(img);
-      this.geometryCon.appendChild(item);
-      this.geometryItems.push(item);
-      this.geometryScale.push(scale);
+    if(this.geometryItems.length === 0){
+      scale = 1;
+      x = 0;
+      y = 0;
+    }else{
+      scale = (Math.random() * 2);
+      x = Math.random() * (this.stageWidth / 2) * getRandomSign();
+      y = Math.random() * (this.stageHeight / 2) * getRandomSign();
     }
-  }
-  moveGeometryCon(e){
-    const x = e.clientX - (this.stageWidth / 2);
-    const y = e.clientY - (this.stageHeight / 2);
 
-    this.geometryItems.forEach((item, index) => {
-      const moveX = -(x / 3);
-      const moveY = -(y / 3);
-      item.style.transform = `scale(${this.geometryScale[index]}) translate(${moveX}px, ${moveY}px)`;
-    })
+    img.classList.add('geometryImg');
+    item.classList.add('geometryItem');
+    item.style.transform = `translate(${x}px, ${y}px) scale(${scale})`;
+
+    item.appendChild(img);
+    this.geometryCon.appendChild(item);
+    this.geometryItems.push(item);
   }
+  // moveGeometryCon(e){
+  //   const x = e.clientX - (this.stageWidth / 2);
+  //   const y = e.clientY - (this.stageHeight / 2);
+
+  //   this.geometryItems.forEach((item, index) => {
+  //     const moveX = -(x / 3);
+  //     const moveY = -(y / 3);
+  //     item.style.transform = `scale(${this.geometryScale[index]}) translate(${moveX}px, ${moveY}px)`;
+  //   })
+  // }
 }
